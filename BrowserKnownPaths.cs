259aa46cf3828aa8c9bb14cf2bd8c9e1d61b8278
Chris <chrisasmall+github@gmail.com>
2025-10-11 22:51:43 +0100

    Fix Brave, Edge, Opera paths adding MacOS

--- BrowserKnownPaths.cs
+++ BrowserKnownPaths.cs
@@ -5,32 +5,32 @@ namespace TinyCity
         public static string ChromePath => Environment.OSVersion.Platform switch
         {
             PlatformID.Unix => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "google-chrome"),
-            PlatformID.MacOSX => throw new NotImplementedException("MacOS path not implemented."),
+            PlatformID.MacOSX => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Library", "Application Support", "Google", "Chrome"),
             PlatformID.Win32NT => Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "Google", "Chrome", "User Data"),
             _ => throw new NotSupportedException("Unsupported platform.")
         };
 
         public static string EdgePath => Environment.OSVersion.Platform switch
         {
-            PlatformID.Unix => throw new NotImplementedException("Linux path not implemented."),
-            PlatformID.MacOSX => throw new NotImplementedException("MacOS path not implemented."),
-            PlatformID.Win32NT => Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "Microsoft", "Edge", "User Data"),
+            PlatformID.Unix => throw new NotImplementedException("Linux path not implemented for Edge."),
+            PlatformID.MacOSX => throw new NotImplementedException("MacOS path not implemented for Edge."),
+            PlatformID.Win32NT => Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "Microsoft", "Edge", "User Data", "Default", "Bookmarks"),
             _ => throw new NotSupportedException("Unsupported platform.")
         };
 
         public static string BravePath => Environment.OSVersion.Platform switch
         {
-            PlatformID.Unix => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "BraveSoftware", "Brave-Browser"),
-            PlatformID.MacOSX => throw new NotImplementedException("MacOS path not implemented."),
-            PlatformID.Win32NT => Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "BraveSoftware", "Brave-Browser", "User Data"),
+            PlatformID.Unix => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "BraveSoftware", "Brave-Browser", "Default", "Bookmarks"),
+            PlatformID.MacOSX => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Library", "Application Support", "BraveSoftware", "Brave-Browser", "Default", "Bookmarks"),
+            PlatformID.Win32NT => Path.Combine(Environment.GetEnvironmentVariable("LOCALAPPDATA"), "BraveSoftware", "Brave-Browser", "User Data", "Default", "Bookmarks"),
             _ => throw new NotSupportedException("Unsupported platform.")
         };
 
         public static string OperaPath => Environment.OSVersion.Platform switch
         {
             PlatformID.Unix => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), ".config", "opera"),
-            PlatformID.MacOSX => throw new NotImplementedException("MacOS path not implemented."),
-            PlatformID.Win32NT => Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), "Opera Software", "Opera Stable", "User Data"),
+            PlatformID.MacOSX => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.UserProfile), "Library", "Application Support", "com.operasoftware.Opera", "Bookmarks"),
+            PlatformID.Win32NT => Path.Combine(Environment.GetEnvironmentVariable("APPDATA"), "Opera Software", "Opera Stable", "User Data", "Default", "Bookmarks"),
             _ => throw new NotSupportedException("Unsupported platform.")
         };
     }
