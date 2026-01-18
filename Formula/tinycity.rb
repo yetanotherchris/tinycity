0e90c93e80e43814d46162149fa2a86c32930fe0
yetanotherchris <54912+yetanotherchris@users.noreply.github.com>
2026-01-18 13:38:18 +0000

    Update scoop and homebrew files to version 3.2.0

--- Formula/tinycity.rb
+++ Formula/tinycity.rb
@@ -1,33 +1,33 @@
 class Tinycity < Formula
   desc "Ask any large language model from your terminal via OpenAI-compatible APIs"
   homepage "https://github.com/yetanotherchris/tinycity"
-  version "3.1.1"
+  version "3.2.0"
   license "MIT"
 
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/yetanotherchris/tinycity/releases/download/v3.1.1/tinycity-v3.1.1-osx-arm64"
-      sha256 "7e11c6959122d9166cb145edad0efa3780eb67dd84d40c54776199836a716a3c"
+      url "https://github.com/yetanotherchris/tinycity/releases/download/v3.2.0/tinycity-v3.2.0-osx-arm64"
+      sha256 "a8864ec06aed0c0437fe941ec990337bb2455e6328a92b5068d5c8825fe7da0d"
     else
-      url "https://github.com/yetanotherchris/tinycity/releases/download/v3.1.1/tinycity-v3.1.1-osx-x64"
-      sha256 "055e0384edefc96cf92a731df3a00941a68c99295128640018af4416a5dcd8d5"
+      url "https://github.com/yetanotherchris/tinycity/releases/download/v3.2.0/tinycity-v3.2.0-osx-x64"
+      sha256 "4d3f91e045caac3f723aaf82445bdb1e52105990fb3524bb7672f8f8fc0cb967"
     end
   end
 
   on_linux do
-    url "https://github.com/yetanotherchris/tinycity/releases/download/v3.1.1/tinycity-v3.1.1-linux-x64"
-    sha256 "b96dcf0027ebc4252b3aae03f3dee0f0be1785838b6efcf1ba2c31a0db7a074f"
+    url "https://github.com/yetanotherchris/tinycity/releases/download/v3.2.0/tinycity-v3.2.0-linux-x64"
+    sha256 "fc9362cc24fbbdaf29a4fd82fcf693abb77e1f5d7282553107953a78163feb5e"
   end
 
   def install
     if OS.mac?
       if Hardware::CPU.arm?
-        bin.install "tinycity-v3.1.1-osx-arm64" => "tinycity"
+        bin.install "tinycity-v3.2.0-osx-arm64" => "tinycity"
       else
-        bin.install "tinycity-v3.1.1-osx-x64" => "tinycity"
+        bin.install "tinycity-v3.2.0-osx-x64" => "tinycity"
       end
     else
-      bin.install "tinycity-v3.1.1-linux-x64" => "tinycity"
+      bin.install "tinycity-v3.2.0-linux-x64" => "tinycity"
     end
   end
 
@@ -35,6 +35,7 @@ class Tinycity < Formula
     assert_match "USAGE:", shell_output("#{bin}/tinycity --help")
   end
 end
+
 
 
 
